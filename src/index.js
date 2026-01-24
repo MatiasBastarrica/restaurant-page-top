d883d73458482f770e7dc1106c2598269b8cec49
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2026-01-24 17:22:32 -0300

    Writ the tab-switching logic

--- src/index.js
+++ src/index.js
@@ -4,6 +4,22 @@ import { loadAbout } from "./about.js";
 import { loadMenu } from "./menu.js";
 
 console.log("Welcome, Odinite!");
-// loadHome();
-loadAbout();
-// loadMenu();
+loadHome();
+
+const tabs = document.querySelectorAll(".tab");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", function (e) {
+    const content = document.querySelector("#content");
+    const childDiv = content.firstChild;
+
+    content.removeChild(childDiv);
+    if (e.target.classList.contains("home-tab")) {
+      loadHome();
+    } else if (e.target.classList.contains("menu-tab")) {
+      loadMenu();
+    } else if (e.target.classList.contains("about-tab")) {
+      loadAbout();
+    }
+  });
+});
