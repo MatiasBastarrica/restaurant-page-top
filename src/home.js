48af573ef91fb14dc0f83f65dfbf235345a09ee7
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2026-01-24 15:53:15 -0300

    Rewrite the "Home" module
    I simplified the module by importing some helper
    functions to make it DRY

--- src/home.js
+++ src/home.js
@@ -1,21 +1,25 @@
 import remyImage from "./remy.webp";
+import { createElement, appendElements } from "./elements.js";
 
 const content = document.querySelector("#content");
+const divParent = createElement("div");
 
-export function loadHomepage() {
-  const title = document.createElement("h1");
-  title.textContent = "La Ratatouille";
+const title = createElement("h1");
+title.textContent = "La Ratatouille";
 
-  const img = document.createElement("img");
-  img.src = remyImage;
-  img.classList.add("homepage-img");
+const img = createElement("img");
+img.src = remyImage;
+img.classList.add("homepage-img");
 
-  const p = document.createElement("p");
-  p.textContent =
-    "Here in La Ratatouille our cheff Remy will cook special dishes that can take you back to your childhood. Even the most severe of the critics can give testimony of the many emotions one can feel when eating in this cozy restaurant.";
-  p.classList.add("description");
+const p = createElement("p");
+p.textContent =
+  "Here in La Ratatouille our cheff Remy will cook special dishes that can take you back to your childhood. Even the most severe of the critics can give testimony of the many emotions one can feel when eating in this cozy restaurant.";
+p.classList.add("description");
 
-  content.appendChild(title);
-  content.appendChild(img);
-  content.appendChild(p);
+const elements = [title, img, p];
+
+appendElements(elements, divParent);
+
+export function loadHome() {
+  content.appendChild(divParent);
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import "./styles.css";
-import { loadHomepage } from "./page-load.js";
+import { loadHome } from "./home.js";
 import { loadAbout } from "./about.js";
 import { loadMenu } from "./menu.js";
 
 console.log("Welcome, Odinite!");
-// loadHomepage();\
+// loadHome();
 // loadAbout();
-loadMenu();
+// loadMenu();
